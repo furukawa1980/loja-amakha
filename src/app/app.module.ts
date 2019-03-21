b9a554c8829d94bc45f3b89bb4bfbb80f5e55bd1
Fabio Furukawa <faboxfurukawa@hotmail.com>
2019-03-21 18:41:18 -0300

    Salvando Amakha

---
+++ src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { AppComponent } from './app.component';
+
+import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
+import { ROUTES } from "./app.routes"
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    HomeComponent
+  ],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot(ROUTES)
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
